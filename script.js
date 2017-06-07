74e0b3a754d1bf6a5c07f6546e8e954ab2b49151
omer257 <omer257@gmail.com>
2017-06-07 15:23:04 +0300

    added google search

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 (function isdnPrj() {
+
+
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
     var source = $('#book-template').html();
     var template = Handlebars.compile(source);
+    var id = getUrlParameter('id');
 
 
-
-    var getData = function () {
-        var item =$('#isbnnumber').val();
+    var getData = function (item) {
+        var LoadingAJax='<img src="http://bestanimations.com/Animals/Mammals/Dogs/dogs/cute-funny-dog-animated-gif-8.gif">';
+        $('.results').empty().html(LoadingAJax)
         $.ajax({
-            url: "https://www.googleapis.com/books/v1/volumes?q=isbn:"+item,
+            url: "https://www.googleapis.com/books/v1/volumes?q=" + item + '&rnd=' + Math.random(),
 
             // The name of the callback parameter, as specified by the YQL service
             jsonp: "callback",
@@ -22,55 +39,37 @@
 
             // Work with the response
             success: function (response) {
-                var data = response.items[0].volumeInfo;
-                console.log(response.items[0].volumeInfo); // server response
-                var newHTML = template({
-                    title: data.title,
-                    publisher: data.publisher,
-                    publishedDate: data.publishedDate,
-                    description: data.description,
-                    infoLink: data.infoLink
-                });
-                $('.results').append(newHTML);
+                $('.results').empty();
+                if (response.items.length > 0) {
+                    response.items.forEach(function (item) {
+                        var data = item.volumeInfo;
+                        var newHTML = template({
+                            title: data.title,
+                            publisher: data.publisher,
+                            publishedDate: data.publishedDate,
+                            description: data.description,
+                            infoLink: data.infoLink,
+                            id: item.id,
+                            image: data.imageLinks.thumbnail || 'http://funnystack.com/wp-content/uploads/2015/07/Funny-Children-32.jpg'
+                        });
+                        $('.results').append(newHTML);
+                    });
+                } else {
+                    $('.results').append('No results found');
+                }
+
+
             },
             error: function (response) {
                 console.log('fcuked'); // server response
             }
         });
     };
-    
-    var getList = function () {
-        $.ajax({
-            url: "https://www.googleapis.com/books/v1/volumes?q=space",
-
-            // The name of the callback parameter, as specified by the YQL service
-            jsonp: "callback",
-
-            // Tell jQuery we're expecting JSONP
-            dataType: "jsonp",
-
-            // Tell YQL what we want and that we want JSON
-            data: {
-                format: "json"
-            },
-
-            // Work with the response
-            success: function (response) {
-                var xx = response;
-                    console.log(xx.items[0]);
-            },
-            error: function (response) {
-                console.log('fcuked'); // server response
-            }
-        });
-    };
-    
-    $(".help").on('click',function () {
-        getList();
-    });
+    if (id !== undefined) {
+        getData(id);
+    }
     $("form").submit(function () {
-        getData();
+        getData($('#isbnnumber').val());
     });
 
 })();
-
